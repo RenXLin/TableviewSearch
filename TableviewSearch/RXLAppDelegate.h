817f42c9d67b7ae912e47c9ebab7f69aafba1be1
qianfeng1 <qianfeng1@RXL.local>
2014-01-08 17:50:20 +0800

    Initial Commit

---
+++ TableviewSearch/RXLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RXLAppDelegate.h
+//  TableviewSearch
+//
+//  Created by qianfeng1 on 14-1-8.
+//  Copyright (c) 2014年 任小林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RXLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableviewSearch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableviewSearch
+//
+//  Created by qianfeng1 on 14-1-8.
+//  Copyright (c) 2014年 任小林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RXLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RXLAppDelegate class]));
+    }
+}
